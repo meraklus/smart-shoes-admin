d297935126a764a3b652e81f01a39b86204a36dc
sangwon <sang5879@naver.con>
2024-07-19 11:11:07 +0900

    커밋

---
+++ src/main/java/com/example/smart_shoes_admin/user/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.smart_shoes_admin.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+
+
+}
