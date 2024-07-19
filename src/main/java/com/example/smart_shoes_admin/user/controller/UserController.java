822fe64b57850645cec8d216585d1f0e283706d5
sangwon <sang5879@naver.con>
2024-07-19 13:25:43 +0900

    커밋

--- src/main/java/com/example/smart_shoes_admin/user/controller/UserController.java
+++ src/main/java/com/example/smart_shoes_admin/user/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.example.smart_shoes_admin.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 public class UserController {
 
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable Long id) {
 
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
 }
